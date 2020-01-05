072c8959499335bd6928e6b1ce16d364f3b6f96a
Isaac McDaniel <mcdaniel.isaac@gmail.com>
2020-01-05 16:06:13 -0600

    Added html and image optimizations

--- themes/chainsaw/source/js/scripts.js
+++ themes/chainsaw/source/js/scripts.js
@@ -54,3 +54,26 @@ window.addeventasync = function(){
     });
 
 };
+
+document.addEventListener("DOMContentLoaded", function() {
+    var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+  
+    if ("IntersectionObserver" in window) {
+      let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(function(entry) {
+          if (entry.isIntersecting) {
+            let lazyImage = entry.target;
+            lazyImage.src = lazyImage.dataset.srcset;
+            lazyImage.classList.remove("lazy");
+            lazyImageObserver.unobserve(lazyImage);
+          }
+        });
+      });
+  
+      lazyImages.forEach(function(lazyImage) {
+        lazyImageObserver.observe(lazyImage);
+      });
+    } else {
+      // Possibly fall back to a more compatible method here
+    }
+  });
